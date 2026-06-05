a6c90a274b4e24d6ec26cc18683443dcab865509
Ahmed Maruf <marufishtiaqueahmed@gmail.com>
2026-06-05 10:45:44 +0200

    chore: add main method

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from download_datasets import download_datasets
 from preprocessing import preprocess_datasets
+from lejepa import pretrain_lejepa_encoder
+
+def main():
+    download_datasets()
+    preprocess_datasets()
+    pretrain_lejepa_encoder()
 
 
-download_datasets()
-preprocess_datasets()
+if __name__ == "__main__":
+    raise SystemExit(main())
